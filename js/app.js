7f4c791cb4fcc7c2f0c51de291c2c31098ae4fa5
matheus felipe <maahgomes.r@gmail.com>
2025-10-20 12:04:43 -0300

    remane variable gameSelected

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 function alterarStatus(valorStatus) {
-    let jogo = document.getElementById(`game-${valorStatus}`);
-    let imgContainer = jogo.querySelector('.dashboard__item__img');
-    let button = jogo.querySelector('.dashboard__item__button');
+    let gameSelected = document.getElementById(`game-${valorStatus}`);
+    let imgContainer = gameSelected.querySelector('.dashboard__item__img');
+    let button = gameSelected.querySelector('.dashboard__item__button');
 
     if (button.classList.contains('dashboard__item__button--return') && imgContainer.classList.contains('dashboard__item__img--rented')) {
         button.classList.remove('dashboard__item__button--return');
